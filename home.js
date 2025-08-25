ee1804d3f5796b1140569193226d6d107babb7b0
shihab1010 <shihabhossen185@gmail.com>
2025-08-26 01:18:05 +0600

    added transfer money, get bonus & pay bill section

--- home.js
+++ home.js
@@ -1,4 +1,51 @@
 const validPin = 1234;
+const validCoupon = "a123";
+//...... reusable function.........
+
+// get value as INT
+function getValueInt(id) {
+  const inputFieldValue = parseInt(document.getElementById(id).value);
+  return inputFieldValue;
+}
+
+// get value as String
+function getValueString(id) {
+  const inputValue = document.getElementById(id).value;
+  return inputValue;
+}
+
+// get Inner text
+
+function getInnerText(id) {
+  const innertext = document.getElementById("avail-balance").innerText;
+  return innertext;
+}
+
+// Set Inner text
+function setInnerText(value) {
+  document.getElementById("avail-balance").innerText = value;
+}
+// for togolling
+
+// id1=> for card green border
+// id2=> for which form show
+function toggoleFeature(id1, id2) {
+  // green border
+  const cards = document.getElementsByClassName("card");
+  for (const card of cards) {
+    card.style.border = "";
+  }
+  document.getElementById(id1).style.border = "2px solid lightgreen";
+
+  // display none
+  const forms = document.getElementsByClassName("form");
+  for (const form of forms) {
+    form.style.display = "none";
+  }
+  document.getElementById(id2).style.display = "block";
+}
+
+// >>>><<<<<<<<<<<<>>>>>>>>>><<<<<<<<<<<<>>>>>>>>><<<<<<<<
 
 // add amount
 
@@ -6,12 +53,10 @@ document
   .getElementById("add-ammount-btn")
   .addEventListener("click", function (e) {
     e.preventDefault();
-    const bank = document.getElementById("bank").value;
-    const accountNumber = document.getElementById("account-number").value;
-    const addAmmount = parseInt(document.getElementById("add-ammount").value);
-    const homePinNumber = parseInt(
-      document.getElementById("home-pin-number").value
-    );
+    const bank = getValueString("bank");
+    const accountNumber = getValueString("account-number");
+    const addAmmount = getValueInt("add-ammount");
+    const homePinNumber = getValueInt("home-pin-number");
 
     if (accountNumber.length < 11) {
       alert("Please give valid account number.");
@@ -32,7 +77,7 @@ document
     );
 
     const newAvailBalance = availbalance + addAmmount;
-    document.getElementById("avail-balance").innerText = newAvailBalance;
+    setInnerText(newAvailBalance);
     alert("Ammount added succesfully.");
   });
 
@@ -41,14 +86,10 @@ document
   .getElementById("withdraw-ammount-btn")
   .addEventListener("click", function (e) {
     e.preventDefault();
-    const withdrawAmount = parseInt(
-      document.getElementById("withdraw-ammount").value
-    );
-    const availbalance = parseInt(
-      document.getElementById("avail-balance").innerText
-    );
-    const agentNumber = document.getElementById("agent-number").value;
-    const pinNumber = parseInt(document.getElementById("withdraw-pin").value);
+    const withdrawAmount = getValueInt("withdraw-ammount");
+    const availbalance = parseInt(getInnerText("avail-balance"));
+    const agentNumber = getValueString("agent-number");
+    const pinNumber = getValueInt("withdraw-pin");
     if (agentNumber.length < 11) {
       alert("Please give valid account number.");
       return;
@@ -69,20 +110,118 @@ document
     }
 
     const newAvailBalance = availbalance - withdrawAmount;
-    document.getElementById("avail-balance").innerText = newAvailBalance;
+    setInnerText(newAvailBalance);
     alert("Ammount withdraw succesfully.");
   });
 
-//   toggoling section
-document.getElementById("add-money").addEventListener("click", function () {
-  document.getElementById("add-money").style.border = "2px solid lightgreen";
-  document.getElementById("cash-out").style.border = "";
-  document.getElementById("cashout-forms").style.display = "none";
-  document.getElementById("add-money-forms").style.display = "block";
+// transfer money
+document
+  .getElementById("transfer-ammount-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const transferAmount = getValueInt("transfer-ammount");
+    const availbalance = parseInt(getInnerText("avail-balance"));
+    const userAccountNumber = getValueString("user-account-number");
+    const pinNumber = getValueInt("transfer-pin");
+    if (userAccountNumber.length < 11) {
+      alert("Please give valid account number.");
+      return;
+    }
+
+    if (pinNumber !== validPin) {
+      alert("PIN is not Correct.");
+      return;
+    }
+
+    if (transferAmount <= 0) {
+      alert("Please give valid ammount.");
+      return;
+    }
+    if (availbalance < transferAmount) {
+      alert("Insufficient balance.");
+      return;
+    }
+
+    const newAvailBalance = availbalance - transferAmount;
+    setInnerText(newAvailBalance);
+    alert("Ammount transfer succesfully.");
+  });
+
+// get bonus
+document.getElementById("bonus-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const couponNumber = getValueString("coupon-number");
+  if (couponNumber != validCoupon) {
+    alert("Coupon is not Correct.");
+    return;
+  }
+  const availbalance = parseInt(getInnerText("avail-balance"));
+  const newAvailBalance = availbalance + 100;
+  setInnerText(newAvailBalance);
+  alert("Bonus added succesfully.");
 });
+
+// pay now
+document.getElementById("pay-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const billerAccountNumber = getValueString("biller-account-number");
+  const payAmount = getValueInt("pay-ammount");
+  const payPinNumber = getValueInt("pay-pin-number");
+  const availbalance = parseInt(getInnerText("avail-balance"));
+
+  if (billerAccountNumber.length < 11) {
+    alert("Please give valid account number.");
+    return;
+  }
+
+  if (payPinNumber !== validPin) {
+    alert("PIN is not Correct.");
+    return;
+  }
+
+  if (payAmount <= 0) {
+    alert("Please give valid ammount.");
+    return;
+  }
+  if (availbalance < payAmount) {
+    alert("Insufficient balance.");
+    return;
+  }
+
+  if (availbalance < payAmount) {
+    alert("Insufficient balance.");
+    return;
+  }
+
+  const newAvailBalance = availbalance - payAmount;
+  setInnerText(newAvailBalance);
+  alert("Paid succesfully.");
+});
+
+//   toggoling section
+
+// for add money
+document.getElementById("add-money").addEventListener("click", function () {
+  toggoleFeature("add-money", "add-money-forms");
+});
+
+// for cash out
 document.getElementById("cash-out").addEventListener("click", function () {
-  document.getElementById("cash-out").style.border = "2px solid lightgreen";
-  document.getElementById("add-money").style.border = "";
-  document.getElementById("add-money-forms").style.display = "none";
-  document.getElementById("cashout-forms").style.display = "block";
+  toggoleFeature("cash-out", "cashout-forms");
+});
+
+// for transfer money
+document
+  .getElementById("transfer-money")
+  .addEventListener("click", function () {
+    toggoleFeature("transfer-money", "transfer-money-forms");
+  });
+
+// for get bonus
+document.getElementById("get-bonus").addEventListener("click", function () {
+  toggoleFeature("get-bonus", "get-bonus-forms");
+});
+// for pay bill
+document.getElementById("pay-bill").addEventListener("click", function () {
+  toggoleFeature("pay-bill", "pay-bill-forms");
 });
